c615d3ba59df8fa31989884c481aeb21c7066d7c
Jeff Fessler <fessler@umich.edu>
2022-11-10 23:33:48 -0500

    Fix b0map test

--- test/b0map.jl
+++ test/b0map.jl
@@ -1,11 +1,17 @@
 # test/b0map.jl
 
-using MRIfieldmap: b0map
+using MRIfieldmap: b0map, b0scale
 using Test: @test, @testset, @inferred
 #using Unitful: todo
-using MIRTjim: jim
 using ImageGeoms: ImageGeom, circle
+using LinearAlgebra: norm
 using Random: seed!
+
+#=
+using MIRTjim: jim
+jim(:prompt, true)
+jif = (args...; kwargs...) -> jim(args...; prompt=false, kwargs...)
+=#
 
 @testset "b0map" begin
 #   @inferred b0map() # not type stable - too many "out" options
@@ -15,50 +21,60 @@ using Random: seed!
     mask = circle(ig)
     # simple test b0 fieldmap and image
     ftrue = -10 .+ 50 * exp.(-0.1f0 * (abs2.(axes(ig)[1]) .+ abs2.(axes(ig)[2]')))
+    rmse = fhat -> round(norm((fhat - ftrue) .* mask) / sqrt(count(mask)); digits=1)
+    flim = (-20, 40)
     xtrue = mask +
         (abs.(axes(ig)[1])/ig.dims[1] .+ abs.(axes(ig)[2]')/ig.dims[2] .< 0.3)
 
-    # single coil case
+#=
+function showit(fhat)
+    jim(jif(ftrue, "ftrue"), jif(mask), jif(xtrue, "xtrue"),
+        jif(finit.*mask, "finit"),
+        jif(fhat, "fhat"; clim=flim, xlabel="$(rmse(fhat))"),
+        jif(fhat-ftrue, "err"),
+    )
+end
+=#
+
+    # single coil 2D case
 
     ydata = [xtrue .* cis.(2f0π * ftrue * ΔTE) for ΔTE in etime]
     ydata = cat(dims=3, ydata...)
     seed!(0)
-    ydata .+= 0.02 * randn(ComplexF32, size(ydata))
+    ydata .+= 0.03 * randn(ComplexF32, size(ydata))
+    ydata, _ = b0scale(ydata, etime)
 
     finit = angle.(ydata[:,:,2] .* conj(ydata[:,:,1])) / (etime[2] - etime[1]) / 2f0π
+    finit .*= mask
 
     for track in (false, true)
-        (fhat, _, _) = b0map(finit, ydata, etime; niter=5, l2b=-3, track, chat=false)
+        (fhat, _, _) = b0map(finit, ydata, etime; mask,
+            niter=5, track, chat=false)
+        @test maximum(abs, (fhat - ftrue) .* mask) < 2
     end
 
     for precon in (:I, :diag, :chol, :ichol)
-        (fhat, _, _) =
-            b0map(finit, ydata, etime; niter=5, l2b=-3, track=false, precon, chat=false)
+        (fhat, _, _) = b0map(finit, ydata, etime; mask,
+            niter=5, track=false, precon, chat=false)
+        @test maximum(abs, (fhat - ftrue) .* mask) < 2
     end
 
-jim(:prompt, true)
-jif = (args...; kwargs...) -> jim(args...; prompt=false, kwargs...)
-
     for gamma_type in (:FR, :PR)
-        (fhat, _, _) =
-            b0map(finit, ydata, etime; niter=15, l2b=-4, mask, track=false, gamma_type, chat=false)
- 
-        @show maximum(abs, (fhat - ftrue) .* mask) #< 5
-        jim(jif(ftrue), jif(finit.*mask, "finit"), jif(mask), jif(fhat), jif(fhat-ftrue), jif(xtrue, "true $gamma_type"))
-#=
-throw()
-=#
+        (fhat, _, _) = b0map(finit, ydata, etime; mask,
+            niter=5, track=false, gamma_type, chat=false)
+        @test maximum(abs, (fhat - ftrue) .* mask) < 2
     end
 
     # water-fat
-    (fhat, _, out) =
-        b0map(finit, ydata, etime; niter=5, l2b=-3, mask, df=500)
-    @test maximum(abs, (fhat - ftrue) .* mask) < 8
+    (fhat, _, out) = b0map(finit, ydata, etime; mask, niter=5, df=500)
+    @test maximum(abs, (fhat - ftrue) .* mask) < 5
     @test fhat isa Matrix{Float32}
     @test out.xw isa Matrix{ComplexF32} # todo: test better
     @test out.xf isa Matrix{ComplexF32}
 
-    # multiple coil case
+
+    # multiple coil 2D case
+
     smap = cat(fill(1f0+2im, ig.dims), fill(3-1im, ig.dims); dims=3) # (dims..., nc)
 
     ydata = [xtrue .* cis.(2f0π * ftrue * ΔTE) for ΔTE in etime]
@@ -68,17 +84,15 @@ throw()
     ydata = smap .* ydata
     seed!(0)
     ydata .+= 0.04 * randn(ComplexF32, size(ydata))
+    ydata, _ = b0scale(ydata, etime)
     finit = angle.(ydata[:,:,1,2] .* conj(ydata[:,:,1,1])) / (etime[2] - etime[1]) / 2f0π
 
-    (fhat, times, out) =
-        b0map(finit, ydata, etime; smap, niter=9, l2b=-2, mask, chat=false)
-    @test maximum(abs, (fhat - ftrue) .* mask) < 8
-end
+    (fhat, times, out) = b0map(finit, ydata, etime; mask, smap,
+        niter=5, chat=false)
+    @test maximum(abs, (fhat - ftrue) .* mask) < 2
 
-#=
-using MIRTjim: jim
-jim(axes(ig), ftrue)
-jim(axes(ig), xtrue)
-jim(angle.(ydata))
-jim(jim(ftrue), jim(finit.*mask), jim(mask), jim(fhat), jim(fhat-ftrue), jim(xtrue))
-=#
+    (fhat, times, out) = b0map(finit, ydata[:,:,1,:], etime; mask,
+        # smap=ones(ComplexF32,ig),
+        niter=9, chat=false)
+    @test maximum(abs, (fhat - ftrue) .* mask) < 2
+end
