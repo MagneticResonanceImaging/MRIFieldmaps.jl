a997cd8dd7e9ca266c0f0cdfc4f74f6438dced4a
Jeff Fessler <fessler@umich.edu>
2022-11-27 18:55:33 -0500

    Update fat-water example

--- docs/lit/examples/03-fat-water.jl
+++ docs/lit/examples/03-fat-water.jl
@@ -35,7 +35,7 @@ import Downloads # todo: use Fetch or DataDeps?
 using MIRT: ir_mri_sensemap_sim
 using Random: seed!
 using StatsBase: mean
-using Unitful: s
+#using Unitful: s
 using Plots; default(markerstrokecolor=:auto, label="")
 
 
@@ -61,47 +61,43 @@ to illustrate that capability of the package,
 but units are not required.
 =#
 
+#src https://github.com/ClaireYLin/regularized-field-map-estimation/blob/main/examples/example_simu_waterfat.m
+
+#src using GoogleDrive: drive_download # todo fails
+
 # ## Read data
-if !@isdefined(data)
-    repo = "https://github.com/ClaireYLin/regularized-field-map-estimation"
-    dataurl = "$repo/blob/main/data/input_object_40sl_3d_epi_snr40.mat?raw=true"
-    data = matread(Downloads.download(dataurl))
+# Original data source: https://www.ismrm.org/workshops/FatWater12/data.htm
+if !@isdefined(ydata)
+    url = "https://github.com/JeffFessler/scratch/blob/main/linsim2.mat?raw=true"
+#   gdrive_folder = "https://drive.google.com/drive/folders/1SpeSIi_h7ulI-gLVpnBit89J4kH40nxs"
+#   gdrive_file = "https://drive.google.com/file/d/1Sv_D0AmPRbjHT1a7oq7evwjNmf7Tyjff"
+#   local_file = "linsim2.mat"
+#src drive_download(gdrive_file, local_file) # fails
+    data = matread(Downloads.download(url))
+#   data = matread(local_file)
+
+    df = vec(data["df"])
+    relamp = vec(data["df"])
+    mask = data["mask"]
+    finit0 = data["finit0"]
+    finit1 = data["finit1"]
+    finit2 = data["finit2"] # after graph cut todo!
+    ftrue = data["ftrue"]
+    water_true = data["water_true"]
+    fat_true = data["fat_true"]
+    echotime = vec(data["echotime"])
+    ydata = data["yik"]
+    ydata = selectdim(ydata, 3, 1) # xycl = 256, 192, 1, 8)
 end;
 
-# # NOT DONE BELOW HERE
-# Extract arrays used in simulation
-if !@isdefined(ftrue)
-    zp = 1:40 # choose subset of slices
-    mask = data["maskR"][:,:,zp]
-    ftrue = (data["in_obj"]["ztrue"][:,:,zp] .* mask) / 2π / 1s # Hz
-    ftrue .*= mask # true field map (in Hz) for simulation
-    mag = data["in_obj"]["xtrue"] .* mask # true baseline magnitude
-    (nx,ny,nz) = size(mag)
-    clim = (-100,100) # display range in Hz
-    jim(ftrue .* mask; clim, title="True fieldmap in Hz (Fig 3d)")
-end
+# plot images
+#jim(imwater)
+#jim(imfat)
+#jim(ftrue)
+#    jim(ftrue .* mask; clim, title="True fieldmap in Hz (Fig 3d)")
+#jim(ydata[:,:,:,:,end], "|data|"; ncol=nz÷2)
 
-# Function for computing RMSE within the mask
-frmse = f -> round(sqrt(sum(abs2, (f - ftrue)[mask]) / count(mask)) * s, digits=1) / s;
-
-
-# Parameters for data generation
-echotime = [0, 2, 10] * 1f-3 * 1s # echo times in sec
-true_thresh = 0.05 # threshold of the true object for determining reconstruction mask
-snr = 24 # noise level in dB
-ne = length(echotime)
-nc = 4; # number of coils in simulation
-
-# ## Simulate sensitivity maps
-# (`rcoil=100` to match matlab default).
-# todo: polynomial approximation?
-if !@isdefined(smap)
-    smap = ir_mri_sensemap_sim(; dims=(nx, ny, nz), ncoil=nc, rcoil=100)
-    div0 = (x::Number,y::Number) -> iszero(y) ? 0 : x/y
-    smap ./= sqrt.(sum(abs2, smap; dims=4)) # normalize by SSoS
-    jim(smap, "|smap|"; ncol=nz÷2)
-end
-
+# todo: compare b0model to ytrue?
 
 #=
 ## Generate simulated image data
@@ -113,30 +109,78 @@ Because `b0model` uses `cis(+phase)`,
 the resulting fieldmap
 may be the negative of the needed for your scanner!
 =#
-ytrue = b0model(ftrue, mag, echotime; smap)
-seed!(0) # matlab and julia will differ
- # compute the noise_std to get the desired SNR
-image_power = 10 * log10(sum(abs2, mag) / (nx*ny*nz)) # in dB
-noise_power = image_power - snr
-noise_std = sqrt(10^(noise_power/10)) / 2 # because complex
-ynoise = Float32(noise_std) * randn(ComplexF32, size(ytrue))
-ydata = ytrue + ynoise; # add the noise to the data
-# Compute the SNR for each echo time to verify
-tmp = [sum(abs2, ytrue[:,:,:,:,i]) / sum(abs2, ynoise[:,:,:,:,i]) for i in 1:ne]
-datasnr = 10 * log10.(tmp)
-
-# Show data magnitude
-jim(ydata[:,:,:,:,end], "|data|"; ncol=nz÷2)
+# ytrue = b0model(ftrue, mag, echotime)
 
 
-# Coil combine image data and scale
-if !@isdefined(yik_sos)
-    yik_sos = sum(conj(smap) .* ydata; dims=4) # coil combine
-    yik_sos = yik_sos[:,:,:,1,:] # (dims..., ne)
-    jim(yik_sos, "|data sos|"; ncol=nz÷2)
-    (yik_sos_scaled, scale) = b0scale(yik_sos, echotime) # todo
-    jim(yik_sos_scaled, "|scaled data|"; ncol=nz÷2)
-end
+
+# rescale
+ydata_scaled, scale = b0scale(ydata, echotime)
+
+#=
+Initialize fieldmap 2: PWLS (todo?)
+printm(' -- PWLS on winit -- ')
+y = reshape(yik,[],nc,ne);
+w1 = winit_pwls_pcg_ls(w0(mask), yik_c(mask,:,:), p.te,...
+        smap_c(mask,:),'order', 2, 'l2b', -1,  ...
+        'niter', 10,'maskR', mask,'precon','ichol',...
+        'gammaType','PR','df',df,'relamp',relAmps);
+# Initialize fieldmap 3: set background pixels to mean of "good" pixels
+winit = w1;
+p.yk_thresh = 0.01;
+good = winit > p.yk_thresh * max(yik(:));
+winit(~good) = mean(winit(good));
+# plot winit
+winit = reshape(winit,[nx,ny,nz]);
+=#
+
+#=
+# graphcut?
+## 0. Graphcut implementation
+algoParams.DO_OT = 0; # Optimal transfer
+algoParams.OT_ITERS = 100; # number of optimal transfer iterations
+algoParams.lambda = 2^(-7);# Regularization parameter
+algoParams.LMAP_EXTRA = 0; # More smoothing for low-signal regions
+algoParams.LMAP_POWER = 0; # Spatially-varying regularization (2 gives ~ uniformn resolution)
+algoParams.NUM_ITERS = 100; # Number of graph cut iterations
+imDataParams.finit = winit/2/pi;
+[outParams,time_gc] = fmap_est_graphcut(imDataParams, algoParams);
+wmap_gc = 2*pi*outParams.fms.*mask;
+argsError_gc = {'GC', time_gc, wmap_gc};
+=#
+
+#=
+## 2. NCG implementation: ichol precon
+niter_cg = 200;
+[out,cost_cg,time_cg] = fmap_est_pcg_ls(winit(mask), yik_c(mask,:,:),p.te, ...
+    smap_c(mask,:),'order', 2, 'l2b', -7, ...
+    'niter', niter_cg,'maskR', mask,'precon','ichol',...
+    'gammaType','PR','df',df,'relamp',relAmps);
+wmap_cg = embed(out.ws,mask);
+imwater_cg = embed(out.xw,mask);
+imfat_cg = embed(out.xf,mask);
+figure(2);subplot(121);im(wmap_cg(:,:,end), 'cg', wlim)
+subplot(122);semilogy(time_cg,cost_cg,'.-k')
+argsError_cg = {'NCG-MLS-IC', time_cg, wmap_cg};
+## RMSE plots
+argsError = {argsError_qm{:};argsError_gc{:}; argsError_cg{:}};
+error = compute_rmsd(argsError, wtrue,'step',20);
+# labels
+grid on
+axis([0,100,0,60])
+xlabel('Time (s)')
+ylabel('RMSE (Hz)')
+=#
+
+
+# # NOT DONE BELOW HERE todo
+
+# Function for computing RMSE within the mask
+# frmse = f -> round(sqrt(sum(abs2, (f - ftrue)[mask]) / count(mask)) * s, digits=1) / s;
+frmse = f -> round(sqrt(sum(abs2, (f - ftrue)[mask]) / count(mask)), digits=1);
+
+# Parameters for data generation
+ne = length(echotime)
+nc = 1; # number of coils in simulation
 
 
 #=
@@ -145,11 +189,29 @@ end
 Compute `finit`
 using phase difference of first two echo times (no smoothing):
 =#
-finit = b0init(ydata, echotime; smap)
+
+clim = (-100,200)
+if false
+    finit = b0init(ydata, echotime; df, relamp) # not so great!
+    fband = maximum(abs, df) # to match claire lin's code
+    finit3 = b0init(ydata, echotime; df, relamp, nf=100, fband) # not great either
+jim(
+ jim(ftrue; clim),
+ jim(finit; clim),
+ jim(finit0; clim),
+#jim(finit1; clim),
+ jim(finit2; clim),
+)
+
+jim(
+ jim(finit2, "finit2"; clim),
+ jim(finit3, "finit3"; clim),
+)
+end
+finit = finit2 # todo for now
 jim(finit .* mask; clim, title="Initial fieldmap in Hz (Fig 3b)",
     xlabel = "RMSE = $(frmse(finit)) Hz")
 
-#src # QM-Huber / NCG for 3D fieldmap estimation
 
 #=
 ## Run NCG
@@ -158,7 +220,7 @@ Run each algorithm twice; once to track rmse and costs, once for timing
 =#
 yik_scale = ydata / scale
 fmap_run = (niter, precon, track; kwargs...) ->
-    b0map(yik_scale, echotime; smap, mask,
+    b0map(yik_scale, echotime; mask, df, relamp,
        order=1, l2b=-4, gamma_type=:PR, niter, precon, track, kwargs...)
 
 function runner(niter, precon; kwargs...)
@@ -178,6 +240,7 @@ if !@isdefined(fmap_cg_n)
         xlabel = "RMSE = $(frmse(fmap_cg_n)) Hz")
 end
 
+throw()
 
 # ### 3. NCG: diagonal preconditioner
 if !@isdefined(fmap_cg_d)
