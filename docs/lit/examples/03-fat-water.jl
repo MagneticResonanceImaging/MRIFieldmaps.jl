4a05a03e9c71f52892ceb9f4f607c0b98876c735
Jeff Fessler <fessler@umich.edu>
2022-11-28 00:16:03 -0500

    Update

--- docs/lit/examples/03-fat-water.jl
+++ docs/lit/examples/03-fat-water.jl
@@ -37,6 +37,7 @@ using Random: seed!
 using StatsBase: mean
 #using Unitful: s
 using Plots; default(markerstrokecolor=:auto, label="")
+jif = (args...; kwargs...) -> jim(args...; prompt=false, kwargs...);
 
 
 # The following line is helpful when running this file as a script;
@@ -56,196 +57,215 @@ in
 "Efficient Regularized Field Map Estimation in 3D MRI"
 by Claire Lin and Jeff Fessler, 2020
 
+This example is based on translating
+[this matlab code](https://github.com/ClaireYLin/regularized-field-map-estimation/blob/main/examples/example_simu_waterfat.m)
+
 This example uses units (seconds)
 to illustrate that capability of the package,
 but units are not required.
 =#
 
-#src https://github.com/ClaireYLin/regularized-field-map-estimation/blob/main/examples/example_simu_waterfat.m
 
-#src using GoogleDrive: drive_download # todo fails
+#=
+## Read data
 
-# ## Read data
-# Original data source: https://www.ismrm.org/workshops/FatWater12/data.htm
+Original data source:
+https://www.ismrm.org/workshops/FatWater12/data.htm
+=#
 if !@isdefined(ydata)
-    url = "https://github.com/JeffFessler/scratch/blob/main/linsim2.mat?raw=true"
-#   gdrive_folder = "https://drive.google.com/drive/folders/1SpeSIi_h7ulI-gLVpnBit89J4kH40nxs"
-#   gdrive_file = "https://drive.google.com/file/d/1Sv_D0AmPRbjHT1a7oq7evwjNmf7Tyjff"
-#   local_file = "linsim2.mat"
-#src drive_download(gdrive_file, local_file) # fails
+    url = "https://github.com/JeffFessler/scratch/blob/main/linsim2.mat?raw=true" # todo
     data = matread(Downloads.download(url))
-#   data = matread(local_file)
+#src gdrive_folder = "https://drive.google.com/drive/folders/1SpeSIi_h7ulI-gLVpnBit89J4kH40nxs"
+#src gdrive_file = "https://drive.google.com/file/d/1Sv_D0AmPRbjHT1a7oq7evwjNmf7Tyjff"
+#src local_file = "linsim2.mat"
+#src drive_download(gdrive_file, local_file) # fails
+#src data = matread(local_file)
 
     df = vec(data["df"])
-    relamp = vec(data["df"])
+    relamp = vec(data["relamp"])
     mask = data["mask"]
     finit0 = data["finit0"]
     finit1 = data["finit1"]
-    finit2 = data["finit2"] # after graph cut todo!
+    finit2 = data["finit2"] # after graph cut method
     ftrue = data["ftrue"]
     water_true = data["water_true"]
     fat_true = data["fat_true"]
     echotime = vec(data["echotime"])
-    ydata = data["yik"]
+    scale = data["scale"]
+    ydata = data["ydata"]
     ydata = selectdim(ydata, 3, 1) # xycl = 256, 192, 1, 8)
+    ytrue = data["ytrue"]
+    ytrue = selectdim(ytrue, 3, 1) # ""
+    @assert abs(norm(ydata)/norm(ytrue)*scale - 1) < 0.003 # check scale
 end;
 
-# plot images
-#jim(imwater)
-#jim(imfat)
-#jim(ftrue)
-#    jim(ftrue .* mask; clim, title="True fieldmap in Hz (Fig 3d)")
-#jim(ydata[:,:,:,:,end], "|data|"; ncol=nz÷2)
 
-# todo: compare b0model to ytrue?
+# Original image data
+ne = length(echotime)
+jim(data["images_original"], "Original |images| for $ne echo times"; ncol=3)
+
 
 #=
-## Generate simulated image data
-This is the multi-coil version,
-for multiple echo times,
-with additive complex Gaussian noise.
+From those images,
+the following water, fat and field maps
+were computed
+using a golden-section search
+to define "ground truth" images
+for this simulation.
+=#
+jim(
+ jif(water_true, "True water component"),
+ jif(fat_true, "True fat component"),
+ jif(ftrue, "True B0 map [Hz] (Fig. 8a)"),
+ jif(mask, "Mask"),
+)
+
+
+#=
+Verify that the simulated image data in Matlab
+matches the model used here.
 
 Because `b0model` uses `cis(+phase)`,
 the resulting fieldmap
 may be the negative of the needed for your scanner!
 =#
-# ytrue = b0model(ftrue, mag, echotime)
+tmp = b0model(ftrue, water_true, echotime; df, relamp, xfat=fat_true)
+@assert maximum(abs, tmp - ytrue) / maximum(abs, ytrue) < 2f-6
+jim(ydata, "Noisy |data| for $ne echo times"; ncol=3)
 
 
+# Rescale data
+# Not needed here because data was already pre-scaled in Matlab
+#src ydata_scaled, scale = b0scale(ydata, echotime); # todo: move into b0map
 
-# rescale
-ydata_scaled, scale = b0scale(ydata, echotime)
+
+# Function for computing RMSE within the mask
+frmse = f -> round(sqrt(sum(abs2, (f - ftrue)[mask]) / count(mask)), digits=1);
+
 
 #=
-Initialize fieldmap 2: PWLS (todo?)
-printm(' -- PWLS on winit -- ')
-y = reshape(yik,[],nc,ne);
-w1 = winit_pwls_pcg_ls(w0(mask), yik_c(mask,:,:), p.te,...
-        smap_c(mask,:),'order', 2, 'l2b', -1,  ...
-        'niter', 10,'maskR', mask,'precon','ichol',...
-        'gammaType','PR','df',df,'relamp',relAmps);
-# Initialize fieldmap 3: set background pixels to mean of "good" pixels
-winit = w1;
-p.yk_thresh = 0.01;
-good = winit > p.yk_thresh * max(yik(:));
-winit(~good) = mean(winit(good));
-# plot winit
-winit = reshape(winit,[nx,ny,nz]);
+## Initial fieldmap estimate
+
+Compute `finit` via
+a discrete maximum-likelihood search.
+(no smoothing).
+=#
+
+
+# Verify Julia vs Matlab initialization.
+if false
+    fband = maximum(abs, df) # bandwidth that matches (dubious?) Matlab version
+    finit = b0init(ydata, echotime; df, relamp, nf=100, fband, threshold=0)
+    finit .*= mask
+    @assert finit ≈ finit0
+end
+
+#=
+Next the Matlab code runs
+a regularized weighted-least squares method "PWLS"
+to get a smooth initial fieldmap.
+This is not yet implemented in Julia.
+
+todo: revisit after better factoring the CG routine
+
+For future reference:
+`w1 = winit_pwls_pcg_ls(w0(mask), yik_c(mask,:,:), p.te,
+        smap_c(mask,:),'order', 2, 'l2b', -1,
+        'niter', 10,'maskR', mask,'precon','ichol',
+        'gammaType','PR','df',df,'relamp',relAmps)`
 =#
 
 #=
-# graphcut?
-## 0. Graphcut implementation
-algoParams.DO_OT = 0; # Optimal transfer
+Finally the Matlab version
+sets background fieldmap pixels to mean of "good" pixels,
+using a small threshold.
+
+We emulate that approach here using `b0init`.
+Here we use the built-in defaults
+in `b0init`
+for `fband`, `nf` and `threshold`. todo
+=#
+
+if !@isdefined(finit_julia)
+    finit_julia = b0init(ydata, echotime; df, relamp, threshold=0.01)
+end
+finit = finit_julia .* mask;
+
+
+# Uncomment the following line
+# to use the Matlab initial field map
+# for trying to reproduce the original plots.
+finit = finit2;
+
+
+# Show initial fieldmap
+clim = (-100,200)
+jie = (f,t) -> jif(f,t; clim, xlabel = "RMSE: $(frmse(f)) Hz")
+jim(
+ jif(ftrue, "ftrue"; clim),
+ jie(finit0, "finit0 Fig 7b"),
+ jie(finit2, "finit2 Fig 7c"),
+ jie(finit, "finit"),
+ jif(finit2-finit, "f2-fi"),
+)
+
+
+#=
+## Graphcut implementation
+
+Not done here
+
+`algoParams.DO_OT = 0; # Optimal transfer
 algoParams.OT_ITERS = 100; # number of optimal transfer iterations
 algoParams.lambda = 2^(-7);# Regularization parameter
 algoParams.LMAP_EXTRA = 0; # More smoothing for low-signal regions
-algoParams.LMAP_POWER = 0; # Spatially-varying regularization (2 gives ~ uniformn resolution)
+algoParams.LMAP_POWER = 0; # Spatially-varying regularization (2 gives ~ uniform resolution)
 algoParams.NUM_ITERS = 100; # Number of graph cut iterations
 imDataParams.finit = winit/2/pi;
 [outParams,time_gc] = fmap_est_graphcut(imDataParams, algoParams);
 wmap_gc = 2*pi*outParams.fms.*mask;
-argsError_gc = {'GC', time_gc, wmap_gc};
+argsError_gc = {'GC', time_gc, wmap_gc};`
 =#
-
-#=
-## 2. NCG implementation: ichol precon
-niter_cg = 200;
-[out,cost_cg,time_cg] = fmap_est_pcg_ls(winit(mask), yik_c(mask,:,:),p.te, ...
-    smap_c(mask,:),'order', 2, 'l2b', -7, ...
-    'niter', niter_cg,'maskR', mask,'precon','ichol',...
-    'gammaType','PR','df',df,'relamp',relAmps);
-wmap_cg = embed(out.ws,mask);
-imwater_cg = embed(out.xw,mask);
-imfat_cg = embed(out.xf,mask);
-figure(2);subplot(121);im(wmap_cg(:,:,end), 'cg', wlim)
-subplot(122);semilogy(time_cg,cost_cg,'.-k')
-argsError_cg = {'NCG-MLS-IC', time_cg, wmap_cg};
-## RMSE plots
-argsError = {argsError_qm{:};argsError_gc{:}; argsError_cg{:}};
-error = compute_rmsd(argsError, wtrue,'step',20);
-# labels
-grid on
-axis([0,100,0,60])
-xlabel('Time (s)')
-ylabel('RMSE (Hz)')
-=#
-
-
-# # NOT DONE BELOW HERE todo
-
-# Function for computing RMSE within the mask
-# frmse = f -> round(sqrt(sum(abs2, (f - ftrue)[mask]) / count(mask)) * s, digits=1) / s;
-frmse = f -> round(sqrt(sum(abs2, (f - ftrue)[mask]) / count(mask)), digits=1);
-
-# Parameters for data generation
-ne = length(echotime)
-nc = 1; # number of coils in simulation
-
-
-#=
-## Initialize fieldmap
-
-Compute `finit`
-using phase difference of first two echo times (no smoothing):
-=#
-
-clim = (-100,200)
-if false
-    finit = b0init(ydata, echotime; df, relamp) # not so great!
-    fband = maximum(abs, df) # to match claire lin's code
-    finit3 = b0init(ydata, echotime; df, relamp, nf=100, fband) # not great either
-jim(
- jim(ftrue; clim),
- jim(finit; clim),
- jim(finit0; clim),
-#jim(finit1; clim),
- jim(finit2; clim),
-)
-
-jim(
- jim(finit2, "finit2"; clim),
- jim(finit3, "finit3"; clim),
-)
-end
-finit = finit2 # todo for now
-jim(finit .* mask; clim, title="Initial fieldmap in Hz (Fig 3b)",
-    xlabel = "RMSE = $(frmse(finit)) Hz")
 
 
 #=
 ## Run NCG
 
-Run each algorithm twice; once to track rmse and costs, once for timing
+Run each algorithm twice;
+once to track RMSE and costs,
+once for timing.
+
+Caution:
+note the need to correct for the data scale factor.
 =#
-yik_scale = ydata / scale
+order, l2b = 1, -4 # todo
+order, l2b = 2, -7 # from Matlab version
 fmap_run = (niter, precon, track; kwargs...) ->
-    b0map(yik_scale, echotime; mask, df, relamp,
-       order=1, l2b=-4, gamma_type=:PR, niter, precon, track, kwargs...)
+    b0map(ydata, echotime; mask, df, relamp, finit,
+       order, l2b, gamma_type=:PR, niter, precon, track, kwargs...)
 
 function runner(niter, precon; kwargs...)
     (fmap, _, out) = fmap_run(niter, precon, true; kwargs...) # tracking run
     (_, times, _) = fmap_run(niter, precon, false; kwargs...) # timing run
-    return (fmap, out.fhats, out.costs, times)
+    return (fmap, out.fhats, out.costs, times, scale*out.xw, scale*out.xf)
 end;
 
 
 # ### 2. NCG: no precon
-if !@isdefined(fmap_cg_n)
+if !@isdefined(fmap_cg_n) || true
     niter_cg_n = 50
-    (fmap_cg_n, fhat_cg_n, cost_cg_n, time_cg_n) = runner(niter_cg_n, :I)
+    (fmap_cg_n, fhat_cg_n, cost_cg_n, time_cg_n, xw_n, xf_n) = runner(niter_cg_n, :I)
 
     pcost = plot(time_cg_n, cost_cg_n, marker=:circle, label="NCG-MLS");
     pi_cn = jim(fmap_cg_n, "CG:I"; clim,
         xlabel = "RMSE = $(frmse(fmap_cg_n)) Hz")
 end
 
-throw()
 
 # ### 3. NCG: diagonal preconditioner
-if !@isdefined(fmap_cg_d)
-    niter_cg_d = 40
-    (fmap_cg_d, fhat_cg_d, cost_cg_d, time_cg_d) = runner(niter_cg_d, :diag)
+if !@isdefined(fmap_cg_d) || true
+    niter_cg_d = 60
+    (fmap_cg_d, fhat_cg_d, cost_cg_d, time_cg_d, xw_d, xf_d) = runner(niter_cg_d, :diag)
 
     plot!(pcost, time_cg_d, cost_cg_d, marker=:square, label="NCG-MLS-D")
     pi_cd = jim(fmap_cg_d, "CG:diag"; clim,
@@ -255,20 +275,22 @@ end
 
 # ### 4. NCG: Cholesky preconditioner
 # (This one may use too much memory for larger images.)
-if !@isdefined(fmap_cg_c)
-    niter_cg_c = 3
-    (fmap_cg_c, fhat_cg_c, cost_cg_c, time_cg_c) = runner(niter_cg_c, :chol)
+if !@isdefined(fmap_cg_c) || true
+if false
+    niter_cg_c = 15
+    (fmap_cg_c, fhat_cg_c, cost_cg_c, time_cg_c, xw_c, xf_c) = runner(niter_cg_c, :chol)
 
     plot!(pcost, time_cg_c, cost_cg_c, marker=:square, label="NCG-MLS-C")
     pi_cc = jim(fmap_cg_c, "CG:chol"; clim,
         xlabel = "RMSE = $(frmse(fmap_cg_c)) Hz")
 end
+end
 
 
 # ### 5. NCG: Incomplete Cholesky preconditioner
-if !@isdefined(fmap_cg_i)
-    niter_cg_i = 14
-    (fmap_cg_i, fhat_cg_i, cost_cg_i, time_cg_i) =
+if !@isdefined(fmap_cg_i) || true
+    niter_cg_i = 40
+    (fmap_cg_i, fhat_cg_i, cost_cg_i, time_cg_i, xw_i, xf_i) =
         runner(niter_cg_i, :ichol; lldl_args = (; memory=20, droptol=0))
 
     plot!(pcost, time_cg_i, cost_cg_i, marker=:square, label="NCG-MLS-IC",
@@ -279,33 +301,72 @@ end
 
 
 # Compare final RMSE values
-frmse.((ftrue, finit, fmap_cg_n, fmap_cg_d, fmap_cg_c, fmap_cg_i))
+frmse.((ftrue, finit, fmap_cg_n, fmap_cg_d, #=fmap_cg_c,=# fmap_cg_i))
 
 # Plot RMSE vs wall time
-prmse = plot(xlabel = "time [s]", ylabel="RMSE [Hz]")
+prmse = plot(xlabel = "time [s]", ylabel="RMSE [Hz]", ylims=(0,100))
 fun = (time, fhat, label) ->
-    plot!(prmse, time, frmse.(eachslice(fhat; dims=4)); label, marker=:circ)
+    plot!(prmse, time, frmse.(eachslice(fhat; dims=3)); label, marker=:circ)
 fun(time_cg_n, fhat_cg_n, "None")
 fun(time_cg_d, fhat_cg_d, "Diag")
 fun(time_cg_c, fhat_cg_c, "Chol")
 fun(time_cg_i, fhat_cg_i, "IC")
 
+
+# Estimated field map and errors
+phat = jim(
+ jif(ftrue, "ftrue"; clim),
+ jif(mask, "mask"),
+ jie(finit, "finit"),
+ jie(fmap_cg_n, "None"),
+ jie(fmap_cg_d, "Diag"),
+ jie(fmap_cg_i, "IC"),
+)
+
+
+# Estimated water map and errors
+wlim = (0, 100)
+pwater = jim(
+ jif(water_true, "Water true"; clim=wlim),
+ jif(mask, "mask"),
+ jif(xw_i, "Water IC"; clim=wlim),
+ jif(abs.(xw_i - water_true), "|Error|"),
+)
+
+
+# Estimated fat map and errors
+flim = (0, 100)
+pfat = jim(
+ jif(fat_true, "Fat true"; clim=flim),
+ jif(mask, "mask"),
+ jif(xf_i, "Fat IC"; clim=flim),
+ jif(abs.(xf_i - fat_true), "|Error|"),
+)
+
+
 #=
 ## Discussion
 
-That final figure is similar to Fig. 4 of the 2020 Lin&Fessler paper,
-after correcting that figure for a
-[factor of π](https://github.com/ClaireYLin/regularized-field-map-estimation).
+The errors are similar to those in the 2020 paper,
+but still seem larger than expected.
+Further investigation is needed.
+todo: probably a scale-factor issue!
+
+The errors are similar even when initializing with `ftrue`.
+
+The RMSE figure is somewhat similar to Fig. 9 of the 2020 Lin&Fessler paper.
 
 This figure was generated in github's cloud,
 where the servers are busily multi-tasking,
 so the compute times per iteration
 can vary widely between iterations and runs.
 
-Nevertheless,
-it is interesting that
-in this Julia implementation
-the diagonal preconditioner
-seems to be
-as effective as the incomplete Cholesky preconditioner.
+Here the incomplete Cholesky preconditioner
+was very effective
+relative to the diagonal preconditioner.
+
+The Cholesky preconditioner also works well here,
+but this is only a 2D problem.
+For a 3D fieldmap,
+it would likely require too much memory.
 =#
